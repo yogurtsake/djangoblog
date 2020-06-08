d87d8b25f546c3b183107df1433b90462a9339b2
yogurtsake <ilovepython3.7.0@gmail.com>
2020-06-07 20:52:32 -0700

    new

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
-
 from users import views as user_views
 
 urlpatterns = [
@@ -31,9 +29,9 @@ urlpatterns = [
         user_views.register,
         name='register'),
     path(
-        'account/',
+        'profile/',
         user_views.profile,
-        name='account'),
+        name='profile'),
     path(
         'login/',
         auth_views.LoginView.as_view(
@@ -52,4 +50,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
